cf6199d8e3b995ea45c545167f4522be34c77b30
荔枝君 <i@lizhijun.me>
2018-05-21 17:17:51 +0800

    添加Des函数

---
+++ Des3.php
@@ -0,0 +1,30 @@
+<?php
+
+class Des3 {
+    // DES3_KEY
+    var $key;
+    // DES3_IV
+    var $iv;
+
+    public function __construct($key = "gO6ufduPBAJXIXWDO2yVPAQv", $iv = "20170901")
+    {
+        if (strlen($key) != 24){
+            throw new \Exception("DES3_KEY长度错误，长度为24");
+        }
+        if (strlen($iv) != 8){
+            throw new \Exception("DES3_IV长度错误，长度为8");
+        }
+        $this->key = $key;
+        $this->iv = $iv;
+    }
+
+
+    public function encrypt($input){
+        return base64_encode(openssl_encrypt($input, "des-ede3-cbc", $this->key, OPENSSL_RAW_DATA, $this->iv));
+    }
+    
+    public function decrypt($encrypted){
+        return openssl_decrypt(base64_decode($encrypted), 'des-ede3-cbc', $this->key, OPENSSL_RAW_DATA, $this->iv);
+    }
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# php-des3
+# php 3DES函数库
 基于php的3DES函数库
+
+## 使用使用方法
+```
+include dirname(__DIR__).'/Des3.php';
+```
